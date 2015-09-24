28272f287e3c885307d414e73c1e1c8825cc4c56
adjivas <adjivas@users.noreply.github.com>
2015-09-25 00:17:42 +0200

    use type from libc and add write method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+extern crate libc;
+
+use libc::types::os::arch::posix88::{
+  pid_t,
+  ssize_t,
+};
+
+use libc::types::os::arch::c95::{
+  c_int,
+  c_long,
+  c_char,
+  size_t,
+};
+
 extern "C" {
-  fn getpid() -> i64;
-  fn ftok(path: *mut i8, id: u32) -> i64;
-  fn msgget(key: i64, msgflg: u32) -> i64;
-  fn msgsnd(id: i64, snd: *mut MsgBuf, len: u32, flag: i32) -> i8;
-  fn msgrcv(id: i64, snd: *mut MsgBuf, len: u32, mtype: i64, flag: i32) -> i64;
+  fn getpid() -> pid_t;
+  fn ftok(path: *mut c_char, id: c_int) -> c_long;
+  fn msgget(key: c_int, msgflg: c_int) -> c_int;
+  fn msgsnd(id: c_int, snd: *mut MsgBuf, len: size_t, flag: c_int) -> c_int;
+  fn msgrcv(id: c_int, snd: *mut MsgBuf, len: size_t, mtype: c_long, flag: c_int) -> ssize_t;
+  fn write(fd: c_int, buf: *const c_char, len: size_t) -> ssize_t;
 }
 
 #[repr(C)]
 struct MsgBuf {
-  mtype: i64,
-  mtext: [i8; Ipc::MSG_BUFF as usize],
+  mtype: c_long,
+  mtext: [c_char; Ipc::MSG_BUFF as usize],
 }
 
 #[repr(C)]
@@ -23,93 +38,126 @@ enum Ipc {
 
 macro_rules! getpid {
   () => ({
-    unsafe { getpid() }
-  })
+    unsafe { getpid() as i32 }
+  });
+}
+
+macro_rules! write {
+  ($text: expr, $len: expr) => ({
+    write!($text, $len, 1)
+  });
+  ($text: expr, $len: expr, $out: expr) => ({
+    match unsafe {
+      write($out, $text as *const c_char, $len)
+    } {
+      -1 => None,
+      xsi => Some(xsi as i32),
+    }
+  });
 }
 
 macro_rules! ftok {
   () => ({
     match unsafe {
-      ftok(b".".as_ptr() as *mut i8, Ipc::TOK_PROJ_ID as u32)
+      ftok(
+        b"/tmp".as_ptr() as *mut i8,
+        Ipc::TOK_PROJ_ID as c_int,
+      )
     } {
       -1 => None,
-      key => Some(key),
+      key => Some(key as u64),
     }
   });
   ($pathname: expr) => ({
     match unsafe {
-      ftok($pathname.as_ptr() as *mut i8, Ipc::TOK_PROJ_ID as u32)
+      ftok(
+        $pathname.as_ptr() as *mut i8,
+        Ipc::TOK_PROJ_ID as c_int
+      )
     } {
       -1 => None,
-      key => Some(key),
+      key => Some(key as u64),
     }
-  })
+  });
 }
 
 macro_rules! msgget {
   ($key: expr) => ({
     match unsafe {
-      msgget($key, Ipc::CREAT as u32 | 0o066)
+      msgget(
+        $key as c_int,
+        Ipc::CREAT as c_int | 0o0666,
+      )
     } {
-      -1 => None,
-      id => Some(id),
+        -1 => None,
+        id => Some(id as i32),
     }
   });
   ($key: expr, $msgfl: expr) => ({
     match unsafe {
-      msgget($key, $msgfl)
-    } {
-      -1 => None,
-      id => Some(id),
-    }
-  })
-}
-
-macro_rules! msgsnd {
-  ($id: expr, $at: expr) => ({
-    match unsafe { msgsnd(
-        $id,
-        &mut *Box::new(MsgBuf {
-          mtype: $at,
-          mtext: ['a' as i8, '\n' as i8],
-        }),
-        Ipc::MSG_BUFF as u32,
-        Ipc::NOWAIT as i32
+      msgget(
+        $key as c_int,
+        $msgfl,
       )
     } {
       -1 => None,
-      xsi => Some(xsi),
+      id => Some(id as i32),
     }
-  })
+  });
+}
+
+macro_rules! msgsnd {
+  ($id: expr, $at: expr, $text: expr) => ({
+    match unsafe {
+      msgsnd(
+        $id as c_int,
+        &mut *Box::new(MsgBuf {
+          mtype: $at as i64,
+          mtext: ['a' as c_char, '\n' as c_char],
+        }),
+        Ipc::MSG_BUFF as size_t,
+        Ipc::NOWAIT as c_int,
+      )
+    } {
+      -1 => None,
+      xsi => Some(xsi as i32),
+    }
+  });
 }
 
 macro_rules! msgrcv {
   ($id: expr) => ({
-    let from: i64 = getpid!();
+    let from: pid_t = getpid!();
 
     msgrcv!($id, from)
   });
   ($id: expr, $from: expr) => ({
     let mut rcv = Box::new(MsgBuf {
-      mtype: $from,
-      mtext: [0i8, 0i8],
+      mtype: $from as c_long,
+      mtext: ['\0' as c_char, '\0' as c_char],
     });
 
     match unsafe {
-      msgrcv($id, &mut *rcv, Ipc::MSG_BUFF as u32, $from, Ipc::NOWAIT as i32)
+      msgrcv(
+        $id as c_int,
+        &mut *rcv,
+        Ipc::MSG_BUFF as size_t,
+        $from as c_long,
+        Ipc::NOWAIT as c_int
+      )
     } {
       -1 => None,
       _ => Some(rcv.mtext),
     }
   });
-
 }
 
 fn main() {
-  let at:i64 = getpid!();
-  let key:i64 = ftok!().unwrap();
-  let id:i64 = msgget!(key).unwrap();
+  let at:i32 = getpid!();
+  let key:u64 = ftok!(b"/nfs/zfs-student-5/users/2013/adjivas").unwrap();
+  let id:i32 = msgget!(key).unwrap();
+  msgsnd!(id, at, b"a\n").unwrap();
+  let msg = msgrcv!(id).unwrap();
 
-  msgsnd!(id, at).unwrap();
-  println!("{:?}", msgrcv!(id).unwrap() );
+  write!(&msg, 2);
 }
